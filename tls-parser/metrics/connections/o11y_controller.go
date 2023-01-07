16459c006dd9aa115a5b1fe57802826976529d2e
k8spacket <k8spacket@gmail.com>
2023-01-07 11:14:21 +0100

    Show information about server TLS certificate chain

--- tls-parser/metrics/connections/o11y_controller.go
+++ tls-parser/metrics/connections/o11y_controller.go
@@ -22,20 +22,20 @@ func TLSParserConnectionsHandler(w http.ResponseWriter, req *http.Request) {
 				return destination
 			}
 		}
-		buildResponse(w, fmt.Sprintf("http://%%s:%s/tlsparser/connections/%s?%s", os.Getenv("K8S_PACKET_TCP_LISTENER_PORT"), idParam, req.URL.Query().Encode()), resultFunc)
+		buildResponse(w, fmt.Sprintf("http://%%s:%s/tlsparser/connections/%s?%s", os.Getenv("K8S_PACKET_TCP_LISTENER_PORT"), idParam, req.URL.Query().Encode()), metrics.TLSDetails{}, resultFunc)
 	} else {
 		resultFunc := func(destination, source []metrics.TLSConnection) []metrics.TLSConnection {
 			return append(destination, source...)
 		}
-		buildResponse(w, fmt.Sprintf("http://%%s:%s/tlsparser/connections/?%s", os.Getenv("K8S_PACKET_TCP_LISTENER_PORT"), req.URL.Query().Encode()), resultFunc)
+		buildResponse(w, fmt.Sprintf("http://%%s:%s/tlsparser/connections/?%s", os.Getenv("K8S_PACKET_TCP_LISTENER_PORT"), req.URL.Query().Encode()), []metrics.TLSConnection{}, resultFunc)
 	}
 }
 
-func buildResponse[T metrics.TLSDetails | []metrics.TLSConnection](w http.ResponseWriter, url string, resultFunc func(d T, s T) T) {
+func buildResponse[T metrics.TLSDetails | []metrics.TLSConnection](w http.ResponseWriter, url string, t T, resultFunc func(d T, s T) T) {
 	var k8spacketIps = k8s.GetPodIPsByLabel("name", os.Getenv("K8S_PACKET_NAME_LABEL_VALUE"))
 
 	var in T
-	var out T
+	out := t
 
 	for _, ip := range k8spacketIps {
 		resp, err := http.Get(fmt.Sprintf(url, ip))
@@ -58,10 +58,5 @@ func buildResponse[T metrics.TLSDetails | []metrics.TLSConnection](w http.Respon
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	if !reflect.ValueOf(out).IsZero() {
-		_ = json.NewEncoder(w).Encode(out)
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("Not Found 404"))
-	}
+	_ = json.NewEncoder(w).Encode(out)
 }
